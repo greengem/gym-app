83715181802f534173a7e96548fbc937bf1c801e
Green Gem <chris_waitt@me.com>
2023-10-03 11:31:07 +0100

    feat: Server Session - Working RSC Session. - Simple test to show Server session is working.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import { getServerSession } from 'next-auth'
 import Image from 'next/image'
 
-export default function Home() {
-  return <div>Home</div>
+export default async function Home() {
+  const session = await getServerSession();
+  
+  return (
+    <>
+      getServerSession Result:
+      {session?.user?.name ? (
+        <div>{session?.user?.name}</div>
+
+      ) : (
+        <div>Not logged in</div>
+      )}
+    </>
+  );
 }
