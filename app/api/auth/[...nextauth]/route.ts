df643612d2a5365250ff0df8dc0f7eb39525a803
Green Gem <chris_waitt@me.com>
2023-10-03 16:31:44 +0100

    Modify auth route to use prisma adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,13 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github";
 
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
 export const authOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID ?? "",
