d0b7d7f8d4335fedee373433331e60af59cef3d6
Green Gem <chris_waitt@me.com>
2023-10-05 10:53:19 +0100

    feat: Landing page parent

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,12 @@
 import { getServerSession } from 'next-auth'
-import Link from "next/link";
+import HeroSection from './HeroSection';
 
-import PageHeading from "./components/PageHeading";
+import Link from "next/link";
 
 export default async function Home() {
   const session = await getServerSession();
   
   return (
-    <>
-      <PageHeading title="Landing Page" />
-      {session?.user?.name ? (
-        <div>
-          <Link href="/dashboard">Go to Dashboard {session?.user?.name}</Link>
-        </div>
-      ) : (
-        <div><Link href="/api/auth/signin">Log in</Link></div>
-      )}
-    </>
+    <HeroSection />
   );
 }
