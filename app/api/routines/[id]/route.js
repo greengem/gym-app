0c9ec586ab8aac2ea00e16312d124f16e120d828
Green Gem <chris_waitt@me.com>
2023-10-11 11:29:05 +0100

    fix: broke a route by removing NextResponse

--- app/api/routines/[id]/route.js
+++ app/api/routines/[id]/route.js
@@ -1,3 +1,4 @@
+import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/route";
