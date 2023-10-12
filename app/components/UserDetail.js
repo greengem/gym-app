a4056fe85d712d6154f2dc7a206bd9727aaf652f
Green Gem <chris_waitt@me.com>
2023-10-12 13:50:57 +0100

    ui: Moved sidebar User Detail

--- app/components/UserDetail.js
+++ app/components/UserDetail.js
@@ -1,21 +1,16 @@
 import { useSession } from 'next-auth/react';
 import { User } from "@nextui-org/user";
-import { Divider } from '@nextui-org/divider';
 
 export default function UserDetail() {
     const { data: session } = useSession();
 
     return (
-        <>
         <User
             name={session?.user?.name}
             description={session?.user?.email}
             avatarProps={{
                 src: session?.user?.image
             }}
-            className='m-2 mb-5'
         />
-        <Divider className='mb-2' />
-        </>
     );
 }
