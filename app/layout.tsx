54e60aef4d0947fb4dbbd34a3a943b04fbaca9e0
Green Gem <chris_waitt@me.com>
2023-10-12 10:43:42 +0100

    Navmenu re-added

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
       <body>
         <SessionProvider session={session}>
           <Providers>
-
+            <NavMenu />
               {children}
           </Providers>
         </SessionProvider>
